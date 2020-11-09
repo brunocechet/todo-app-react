82a645184a8abc49473d8ef14ed7ee3e1a579fd6
Bruno Cechet <brunocechet@gmail.com>
2020-11-08 21:48:26 -0300

    Adjusting entry point app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
 import React from 'react';
 
-import logo from './logo.svg';
-import './App.css';
+// Routing
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+// Material
+import { Container, CssBaseline } from '@material-ui/core';
+
+// Container Components
+import Tasks from './pages/Tasks';
+import Users from './pages/Users';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <CssBaseline />
+      <Container>
+        <Router basename="/todos">
+          <Switch>
+            <Route path="/tasks/:user">
+              <Tasks />
+            </Route>
+            <Route path="/">
+              <Users />
+            </Route>
+          </Switch>
+        </Router>
+      </Container>
+    </>
   );
 }
 
